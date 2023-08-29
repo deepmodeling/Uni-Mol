b640dda4397f276d0873d74b332d7e4d1e9454f6
Gabriel Wu <qqbbnease1004@126.com>
2023-08-29 09:03:41 +0800

    Fix download.sh (#151)
    A missing quote mark corrupts the download script.

--- unimol_plus/scripts/download.sh
+++ unimol_plus/scripts/download.sh
@@ -2,6 +2,6 @@ wget http://ogb-data.stanford.edu/data/lsc/pcqm4m-v2-train.sdf.tar.gz
 md5sum pcqm4m-v2-train.sdf.tar.gz # fd72bce606e7ddf36c2a832badeec6ab
 tar -xf pcqm4m-v2-train.sdf.tar.gz # extracted pcqm4m-v2-train.sdf
 
-wget 'https://dgl-data.s3-accelerate.amazonaws.com/dataset/OGB-LSC/pcqm4m-v2.zip
+wget 'https://dgl-data.s3-accelerate.amazonaws.com/dataset/OGB-LSC/pcqm4m-v2.zip'
 unzip pcqm4m-v2.zip
 
