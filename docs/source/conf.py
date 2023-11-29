d443685a05b3d5154b770117544c6af3f7f5e27f
cuiyaning <cuiyaning@dp.tech>
2023-11-29 04:51:31 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 html_static_path = ['_static']
