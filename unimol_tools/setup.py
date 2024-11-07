22fb2f9a87fd66af283e34b97fcc4cf54154bdc3
cuiyaning <cuiyaning@dp.tech>
2024-11-07 07:01:13 +0000

    update verion number

--- unimol_tools/setup.py
+++ unimol_tools/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="unimol_tools",
-    version="0.1.0.post4",
+    version="0.1.1",
     description=("unimol_tools is a Python package for property prediciton with Uni-Mol in molecule, materials and protein."),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
